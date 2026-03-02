275e4e8930852f27f5c77edf304241a72776d1bf
laura <laura.carbajal9912@alumnos.udg.mx>
2026-03-02 11:30:11 -0600

    WWW

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,16 +36,25 @@ export default function RootLayout({
     <html lang="es">
       <body className={`${inter.className} font-sans antialiased`}>
         {/* Google Tag Manager */}
-        <Script id="gtm-init" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TFSH9C4P');
-          `}
-        </Script>
-
+        <Script
+            id="gtm-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TFSH9C4P');`
+            }}
+          />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TFSH9C4P"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Suspense fallback={<Loading />}>
           <Navigation />
           <main className="min-h-screen">{children}</main>
