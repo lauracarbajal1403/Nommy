a8a053e11e603af68f12a91e0a90ae10eb23bd06
laura <laura.carbajal9912@alumnos.udg.mx>
2026-03-16 17:23:02 -0600

    dec

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,10 +47,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <GoogleTagManager gtmId="G-YYRDPQQVKE" />
-      <body className={`${inter.className} font-sans antialiased`}>
+      <head>
         <script type="text/javascript" dangerouslySetInnerHTML={{ __html: linkedInScript1 }} />
         <script type="text/javascript" dangerouslySetInnerHTML={{ __html: linkedInScript2 }} />
+      </head>
+      <body className={`${inter.className} font-sans antialiased`}>
+        {/* Google Tag Manager - debe ir al inicio del body */}
+        <GoogleTagManager gtmId="GTM-TFSH9C4P" />
+
+        {/* GTM noscript fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TFSH9C4P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* LinkedIn noscript fallback */}
         <noscript>
           <img
             height="1"
@@ -60,6 +75,7 @@ export default function RootLayout({
             src="https://px.ads.linkedin.com/collect/?pid=8802034&fmt=gif"
           />
         </noscript>
+
         <Suspense fallback={<Loading />}>
           <Navigation />
           <main className="min-h-screen">{children}</main>
